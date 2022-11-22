b1debd217e348ae1b0348c933db8d7bf5dff2ae8
RaynaudPaul <raynaud.pau@educ-valadon-limoges.fr>
2022-11-22 10:32:49 +0100

    Fini dao, cliendatabase et clientrepository

--- app/src/main/java/com/equipe3/compteur/dao/ClientRepository.java
+++ app/src/main/java/com/equipe3/compteur/dao/ClientRepository.java
@@ -11,11 +11,32 @@ public class ClientRepository {
     private LiveData<List<Client>> mAllClients;
 
     public ClientRepository(Application application){
-        IClientDao db = IClientDao.getdatabase(application);
-        mClientDao = db.clientDao();
+        Clientdatabase db = Clientdatabase.getDatabase(application);
+        mClientDao = db.clientdao();
     }
 
-    public LiveData<List<Client>> getmAllClients
+    public LiveData<List<Client>> getmAllClients(){
+      mAllClients = mClientDao.getAlphabetizedClients();
+      return mAllClients;
+    }
+
+    public void insert (Client client){
+        Clientdatabase.databaseWriteExecutor.execute(() ->{
+            mClientDao.insert(client);
+        });
+    }
+
+    public void update(Client client){
+        Clientdatabase.databaseWriteExecutor.execute(() ->{
+            mClientDao.update(client);
+        });
+    }
+
+    public void delete(Client client){
+        Clientdatabase.databaseWriteExecutor.execute(() ->{
+            mClientDao.delete(client);
+        });
+    }
 }
 
 
